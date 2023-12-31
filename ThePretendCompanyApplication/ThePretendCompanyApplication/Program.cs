8a4865732d129b7ef22519646d103c0f10e49d64
JiheiBot <2757174746@qq.com>
2023-12-31 22:47:44 +0000

    finishUp basic linq querys
    where we explored how to use sql like query to get anonmymous data types

--- ThePretendCompanyApplication/ThePretendCompanyApplication/Program.cs
+++ ThePretendCompanyApplication/ThePretendCompanyApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 using TCPData;
@@ -25,6 +26,7 @@ namespace ThePretendCompanyApplication
                 Console.WriteLine();
             }
             */
+            /*
             List<Department> departmentList = Data.GetDepartments();
 
             var filteredDepartments = departmentList.Filter<Department>(d => d.Id > 1);
@@ -36,6 +38,56 @@ namespace ThePretendCompanyApplication
                 Console.WriteLine($"Long Name: {department.LongName}");
                 Console.WriteLine();
             }
+            */
+            /* linq where which is same to custom filter class
+            List<Department> departmentList = Data.GetDepartments();
+
+            var filteredDepartments = departmentList.Where(d => d.ShortName == "TE" || d.ShortName == "HR");
+
+
+            foreach (var department in filteredDepartments)
+            {
+                Console.WriteLine($"Id: {department.Id}");
+                Console.WriteLine($"Short Name: {department.ShortName}");
+                Console.WriteLine($"Long Name: {department.LongName}");
+                Console.WriteLine();
+            }*/
+
+            List<Employee> employeeList = Data.GetEmployees();
+            List<Department> departmentList = Data.GetDepartments();
+
+            //query form
+            var resultList = from emp in employeeList
+                             join dept in departmentList
+                             on emp.DepartmentId equals dept.Id
+                             select new
+                             {
+                                 FirstName = emp.FirstName,
+                                 LastName = emp.LastName,
+                                 AnnualSalary = emp.AnnualSalary,
+                                 Manager = emp.IsManager,
+                                 Department = dept.LongName
+                             };
+
+            foreach (var employee in resultList)
+            {
+                Console.WriteLine($"First Name: {employee.FirstName}");
+                Console.WriteLine($"Last Name: {employee.LastName}");
+                Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+                Console.WriteLine($"Manager: {employee.Manager}");
+                Console.WriteLine($"Manager: {employee.Department}");
+                Console.WriteLine();
+            }
+
+            var averageAnnualSalary = resultList.Average(a => a.AnnualSalary);
+
+            var highestAnnualSalary = resultList.Max(a => a.AnnualSalary);
+
+            var lowestAnnualSalary = resultList.Min(a => a.AnnualSalary);
+
+            Console.WriteLine($"Average Annual Salary: {averageAnnualSalary}");
+            Console.WriteLine($"Highest Annual Salary: {highestAnnualSalary}");
+            Console.WriteLine($"Lowest Annual Salary: {lowestAnnualSalary}");
 
             Console.ReadKey();
         }
