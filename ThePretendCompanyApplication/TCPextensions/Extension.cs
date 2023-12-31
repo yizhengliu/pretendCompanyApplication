f9c5fd0a4fac2f2c7d7913b5183e155d52528187
JiheiBot <2757174746@qq.com>
2023-12-31 21:56:20 +0000

    Explore linq
    We have firstly explored how to implement extension methods. where we can simulate linq commands by using lambdas and func or delegate functions

---
+++ ThePretendCompanyApplication/TCPextensions/Extension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPextensions
+{
+    public static class Extension
+    {
+        public static List<T> Filter<T>(this List<T> records, Func<T, bool> func)
+        {
+            List<T> filetedList = new List<T>();
+
+            foreach (T record in records)
+            {
+                if (func(record))
+                    filetedList.Add(record);
+            }
+            return filetedList;
+        }
+    }
+}
---
+++ ThePretendCompanyApplication/ThePretendCompanyApplication/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCPData;
+using TCPextensions;
+
+namespace ThePretendCompanyApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            List<Employee> employeeList = Data.GetEmployees();
+            var filteredEmployees = employeeList.Filter<Employee>(e => e.AnnualSalary < 50000);
+
+            foreach (var employee in filteredEmployees) 
+            {
+                Console.WriteLine($"First Name: {employee.FirstName}");
+                Console.WriteLine($"Last Name: {employee.LastName}");
+                Console.WriteLine($"Annual Salary: {employee.AnnualSalary}");
+                Console.WriteLine($"Manager: {employee.IsManager}");
+                Console.WriteLine();
+            }
+            */
+            List<Department> departmentList = Data.GetDepartments();
+
+            var filteredDepartments = departmentList.Filter<Department>(d => d.Id > 1);
+
+            foreach (var department in filteredDepartments)
+            {
+                Console.WriteLine($"Id: {department.Id}");
+                Console.WriteLine($"Short Name: {department.ShortName}");
+                Console.WriteLine($"Long Name: {department.LongName}");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
